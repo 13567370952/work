db7afe59e7940ebedd0b31f59d82749499daea0a
xiaochenggit <707996290@qq.com>
2016-04-09 08:21:13 +0800

    rt
    rt

--- 微盟首页/js/js.js
+++ 微盟首页/js/js.js
@@ -1,6 +1,34 @@
 $(document).ready(function(){
 	hd();
+  cpzx();
 });
+function cpzx(){
+  var now = 0;
+  function go(num){
+    $(".cpzxx .ye").css({"display":"none"}).eq(num).css({"display":"block"});
+    $(".cpzxx .dian li").removeClass("active").eq(num).addClass('active');
+  }
+  $(".cpzxx .zuo").click(function(){
+    now--;
+    if(now<0){
+      now=1;
+    }
+    go(now);
+  })
+  $(".cpzxx .you").click(function(){
+    now++;
+    if(now>1){
+      now=0;
+    }
+    go(now);
+  })
+  $(".cpzxx .dian li").each(function(index) {
+    $(this).click(function() {
+      now = index;
+      go(index);
+    });
+  });
+}
 function hd(){
 	var now = 0;
 	function goToSlide(num){
