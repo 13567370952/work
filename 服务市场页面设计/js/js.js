967178ac3b5a5a722ab8d9e62c5037eaf2791410
xiaochenggit <707996290@qq.com>
2016-04-16 08:37:06 +0800

    rt
    rt

--- 服务市场页面设计/js/js.js
+++ 服务市场页面设计/js/js.js
@@ -1,6 +1,33 @@
 $(function () {
 	pxqh()
+	ip()
 })
+function ip(){
+	$(".xdyzt input:checkbox").css({"display":"none"});
+	$(".xdyzt input:radio").css({"display":"none"});
+	if($(".xdyzt .dz input").attr("checked")){
+		 $(".xdyzt .dz label").addClass("yd");
+	}
+	$(".xdyzt .dzz label").each(function(index){
+		$(this).click(function(){
+			$(".xdyzt .dz label").removeClass("yd");
+			$(".xdyzt .dzz label").removeClass("yd").eq(index).addClass("yd");
+		})
+	})
+	$(".xdyzt .dz label").click(function(){
+		$(".xdyzt .dzz label").removeClass("yd");
+		$(this).addClass("yd");
+	})
+	$(".xdyzt .ddbz label").click(function(){
+		if($(".xdyzt .ddbz label").hasClass("yd")){
+			$(this).removeClass("yd");
+		}
+		else{
+			$(this).addClass("yd");
+		}
+		return false;
+	})
+}
 function pxqh(){
 	$(".fwsczt .left li").each(function(index){
 		$(this).click(function(){
